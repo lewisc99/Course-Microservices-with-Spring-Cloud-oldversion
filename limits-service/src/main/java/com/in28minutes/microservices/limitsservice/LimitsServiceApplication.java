69e9e85b96f14867456261f322c3af51eddf5bd2
lewisc99 <lewiscontato99@gmail.com>
2022-09-26 21:08:17 -0300

    add config server client to limits-service

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,6 +3,9 @@ package com.in28minutes.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class LimitsServiceApplication {
 
