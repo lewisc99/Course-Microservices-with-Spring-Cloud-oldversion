2c09b95122566a19ede772a0e84fb150759a293d
lewisc99 <lewiscontato99@gmail.com>
2022-09-28 10:50:59 -0300

    82. Step 15 - Setting up Dynamic Port in the the Response And settings more than one instance of server

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 
 import com.in28minutes.microservices.currencyexchangeservice.models.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,15 @@ import java.math.BigDecimal;
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
 
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)
     {
 
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65), Integer.parseInt(environment.getProperty("local.server.port")));
 
     }
 
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/models/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/models/ExchangeValue.java
@@ -8,15 +8,17 @@ public class ExchangeValue implements Serializable {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private int port;
 
 
     public  ExchangeValue(){}
 
-    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple, int port) {
         this.id = id;
         this.from = from;
         this.to = to;
         this.conversionMultiple = conversionMultiple;
+        this.port = port;
     }
 
     public Long getId() {
@@ -49,5 +51,13 @@ public class ExchangeValue implements Serializable {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
