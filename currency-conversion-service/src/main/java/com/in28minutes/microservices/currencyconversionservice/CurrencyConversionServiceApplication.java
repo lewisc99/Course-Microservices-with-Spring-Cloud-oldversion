0f36adb1610eda432f34ec73bd3b72e4bea41f53
lewisc99 <lewiscontato99@gmail.com>
2022-10-04 16:51:06 -0300

    96. Step 26 - Connecting Currency Conversion Microservice to Eureka

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,14 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+
+@EnableDiscoveryClient
+//@EnableEurekaClient latest annotation to use in the version two
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
